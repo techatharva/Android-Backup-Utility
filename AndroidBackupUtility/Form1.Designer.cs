2782ae18b4da1e1fe497831e3b76aed3bcf67af4
Atharva <92643971+techatharva@users.noreply.github.com>
2024-01-05 21:26:25 +0530

    Add files via upload

---
+++ AndroidBackupUtility/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace AndroidBackupUtility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Restore Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Backup";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Search for Device";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(454, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ANDROID BACKUP UTILITY";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AndroidBackupUtility.Properties.Resources.logo;
+            this.pictureBox2.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(163, 157);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 290);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(488, 228);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label2.Location = new System.Drawing.Point(9, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Device Status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label3.Location = new System.Drawing.Point(34, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Alpha 0.2";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.linkLabel1.Location = new System.Drawing.Point(102, 209);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(165, 15);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Give Feedback / Report Bugs";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(382, 231);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Backup WhatsApp® [ALPHA VERSION]";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 530);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Android Backup Utility";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ AndroidBackupUtility/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.AccessControl;
+
+namespace AndroidBackupUtility
+{
+    public partial class Form1 : Form
+    {
+        private Process process = new Process();
+        private ProcessStartInfo info = new ProcessStartInfo();
+
+        private string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.CreateNoWindow = true;
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+        }
+
+        private void DCIMCopy() 
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/DCIM {path}/DCIM";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+        private void DocumentsCopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Documents {path}/Documents";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+        private void DownloadCopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Download {path}/Download";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+        private void MovCopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Movies {path}/Movies";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void MusicCopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Music {path}/Music";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void PicCopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Pictures {path}/Pictures";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+            MessageBox.Show("Backup completed");
+        }
+
+        private void WACopy()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"pull /sdcard/Android/Media/com.whatsapp/WhatsApp {path}/Android/Media/WhatsApp";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+            {
+                try
+                {   
+                    info.FileName = "adb.exe";
+                    info.Arguments = "devices";
+                    process.StartInfo = info;
+
+                    process.Start();
+
+                    textBox1.Text += process.StandardOutput.ReadToEnd(); 
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowse = new FolderBrowserDialog();
+            folderBrowse.Description = "Select the folder to copy backed up files";
+            if (folderBrowse.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowse.SelectedPath;
+                DCIMCopy();
+                DocumentsCopy();
+                DownloadCopy();
+                MovCopy();
+                MusicCopy();
+                PicCopy();
+            }
+            else
+            {
+                MessageBox.Show("Backup cancelled");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string message = "This only works on Android 10 and above. Do you want to continue?";
+            string title = "Check Android Version";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                WACopy();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string message = "e-Mail me on techatharvaofficial@gmail.com for bugs and suggestions.";
+            string title = "Reports";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void DCIMPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/DCIM /sdcard/DCIM";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+        private void DocumentsPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/Documents /sdcard/Documents";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+        private void DownloadPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/Download /sdcard/Download";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void MovPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/Movies /sdcard/Movies";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void MusicPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/Music /sdcard/Music";
+            process.StartInfo = info;
+
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+        }
+
+        private void PicPush()
+        {
+            info.FileName = "adb.exe";
+            info.Arguments = $"push {path}/Pictures /sdcard/Pictures";
+            process.StartInfo = info;
+            
+            process.Start();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+            MessageBox.Show("Restore completed");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowse = new FolderBrowserDialog();
+            folderBrowse.Description = "Select the backup folder to restore from";
+            if (folderBrowse.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowse.SelectedPath;
+                DCIMCopy();
+                DocumentsCopy();
+                DownloadCopy();
+                MovCopy();
+                MusicCopy();
+                PicCopy();
+            }
+            else
+            {
+                MessageBox.Show("Restore cancelled");
+            }
+        }
+    }
+}
